0609f1ad9edbba3393e79fec097d0a98f5a01735
Mandragora <dan.investor.1996@gmail.com>
2023-02-10 22:39:26 +0100

    Update query.go

--- x/erc20/client/cli/query.go
+++ x/erc20/client/cli/query.go
@@ -7,7 +7,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/spf13/cobra"
 
-	"github.com/ArableProtocol/acrechain/x/erc20/types"
+	"github.com/McDaan/testchain/x/erc20/types"
 )
 
 // GetQueryCmd returns the parent command for all erc20 CLI query commands
