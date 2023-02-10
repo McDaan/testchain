211858c754c48e9386bec4ab723293e9bd28eb34
Mandragora <dan.investor.1996@gmail.com>
2023-02-10 23:05:09 +0100

    Update keeper.go

--- x/mint/keeper/keeper.go
+++ x/mint/keeper/keeper.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/tendermint/tendermint/libs/log"
 
-	"github.com/ArableProtocol/acrechain/x/mint/types"
+	"github.com/McDaan/testchain/x/mint/types"
 
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
