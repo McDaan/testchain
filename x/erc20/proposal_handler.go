1a83e983eac11df6e9331d2266b7a816f95f3c9c
Mandragora <dan.investor.1996@gmail.com>
2023-02-10 22:08:52 +0100

    Update proposal_handler.go

--- x/erc20/proposal_handler.go
+++ x/erc20/proposal_handler.go
@@ -6,8 +6,8 @@ import (
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 	"github.com/ethereum/go-ethereum/common"
 
-	"github.com/ArableProtocol/acrechain/x/erc20/keeper"
-	"github.com/ArableProtocol/acrechain/x/erc20/types"
+	"github.com/McDaan/testchain/x/erc20/keeper"
+	"github.com/McDaan/testchain/x/erc20/types"
 )
 
 // NewErc20ProposalHandler creates a governance handler to manage new proposal types.
