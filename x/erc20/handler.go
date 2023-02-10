bf2a4032a1f7451bbbdeaa879a7c9ec8cefba827
Mandragora <dan.investor.1996@gmail.com>
2023-02-10 22:07:04 +0100

    Update handler.go

--- x/erc20/handler.go
+++ x/erc20/handler.go
@@ -4,7 +4,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
-	"github.com/ArableProtocol/acrechain/x/erc20/types"
+	"github.com/McDaan/testchain/x/erc20/types"
 )
 
 // NewHandler defines the erc20 module handler instance
