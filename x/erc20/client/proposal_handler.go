4357821d4a0a232f7e23fbc6a1fb9eb360689f67
Mandragora <dan.investor.1996@gmail.com>
2023-02-10 22:38:32 +0100

    Update proposal_handler.go

--- x/erc20/client/proposal_handler.go
+++ x/erc20/client/proposal_handler.go
@@ -3,8 +3,8 @@ package client
 import (
 	govclient "github.com/cosmos/cosmos-sdk/x/gov/client"
 
-	"github.com/ArableProtocol/acrechain/x/erc20/client/cli"
-	"github.com/ArableProtocol/acrechain/x/erc20/client/rest"
+	"github.com/McDaan/testchain/x/erc20/client/cli"
+	"github.com/McDaan/testchain/x/erc20/client/rest"
 )
 
 var (
