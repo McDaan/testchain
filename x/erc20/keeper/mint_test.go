fc574c1799eeab912d91dd1289a715e2ed3fa9c5
Mandragora <dan.investor.1996@gmail.com>
2023-02-10 22:48:40 +0100

    Update mint_test.go

--- x/erc20/keeper/mint_test.go
+++ x/erc20/keeper/mint_test.go
@@ -7,7 +7,7 @@ import (
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/evmos/ethermint/tests"
 
-	"github.com/ArableProtocol/acrechain/x/erc20/types"
+	"github.com/McDaan/testchain/x/erc20/types"
 )
 
 func (suite *KeeperTestSuite) TestMintingEnabled() {
