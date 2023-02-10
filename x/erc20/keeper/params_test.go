2ffb44fe52342471cf8b3c8819022fc9606d2d3b
Mandragora <dan.investor.1996@gmail.com>
2023-02-10 22:52:28 +0100

    Update params_test.go

--- x/erc20/keeper/params_test.go
+++ x/erc20/keeper/params_test.go
@@ -1,6 +1,6 @@
 package keeper_test
 
-import "github.com/ArableProtocol/acrechain/x/erc20/types"
+import "github.com/McDaan/testchain/x/erc20/types"
 
 func (suite *KeeperTestSuite) TestParams() {
 	params := suite.app.Erc20Keeper.GetParams(suite.ctx)
