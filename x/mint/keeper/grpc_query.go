78eaf415f1774a095f33eaf1fe4307f34730b63f
Mandragora <dan.investor.1996@gmail.com>
2023-02-10 23:04:33 +0100

    Update grpc_query.go

--- x/mint/keeper/grpc_query.go
+++ x/mint/keeper/grpc_query.go
@@ -5,7 +5,7 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
-	"github.com/ArableProtocol/acrechain/x/mint/types"
+	"github.com/McDaan/testchain/x/mint/types"
 )
 
 var _ types.QueryServer = Querier{}
