1bae8cf55bb6a715d84c2d58a509bd9b07837543
Mandragora <dan.investor.1996@gmail.com>
2023-02-10 19:45:36 +0100

    Update cmd_test.go

--- cmd/acred/cmd_test.go
+++ cmd/acred/cmd_test.go
@@ -17,7 +17,7 @@ func TestInitCmd(t *testing.T) {
 	rootCmd, _ := testd.NewRootCmd()
 	rootCmd.SetArgs([]string{
 		"init",      // Test the init cmd
-		"acre-test", // Moniker
+		"testchain-test", // Moniker
 		fmt.Sprintf("--%s=%s", cli.FlagOverwrite, "true"), // Overwrite genesis.json, in case it already exists
 		fmt.Sprintf("--%s=%s", flags.FlagChainID, "testchain-1"),
 	})
