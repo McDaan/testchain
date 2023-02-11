30f9bf59211c320f3e3dc4b81b17bc8f928ede91
Mandragora <dan.investor.1996@gmail.com>
2023-02-11 02:23:46 +0100

    Update cmd_test.go

--- cmd/testd/cmd_test.go
+++ cmd/testd/cmd_test.go
@@ -19,7 +19,7 @@ func TestInitCmd(t *testing.T) {
 		"init",      // Test the init cmd
 		"testchain-test", // Moniker
 		fmt.Sprintf("--%s=%s", cli.FlagOverwrite, "true"), // Overwrite genesis.json, in case it already exists
-		fmt.Sprintf("--%s=%s", flags.FlagChainID, "testchain-1"),
+		fmt.Sprintf("--%s=%s", flags.FlagChainID, "testchain_9100-1"),
 	})
 
 	err := svrcmd.Execute(rootCmd, app.DefaultNodeHome)
