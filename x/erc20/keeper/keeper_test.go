68f0b44342dfe71ada26380d103a23a45f0e2d01
Mandragora <dan.investor.1996@gmail.com>
2023-02-11 01:56:46 +0100

    Update keeper_test.go

--- x/erc20/keeper/keeper_test.go
+++ x/erc20/keeper/keeper_test.go
@@ -100,7 +100,7 @@ func (suite *KeeperTestSuite) DoSetupTest(t require.TestingT) {
 
 	suite.ctx = suite.app.BaseApp.NewContext(checkTx, tmproto.Header{
 		Height:          1,
-		ChainID:         "bamboo_9051-1",
+		ChainID:         "testchain_9100-1",
 		Time:            time.Now().UTC(),
 		ProposerAddress: suite.consAddress.Bytes(),
 		Version: tmversion.Consensus{
