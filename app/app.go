6a70b727ee5f4b873fe4ef666403c076039111bc
Mandragora <dan.investor.1996@gmail.com>
2023-02-13 01:11:38 +0100

    Update app.go

--- app/app.go
+++ app/app.go
@@ -111,7 +111,7 @@ import (
 	_ "github.com/McDaan/testchain/client/docs/statik"
 
 	"github.com/McDaan/testchain/app/ante"
-	tapp "github.com/McDaan/testchain/app/handler_options.go"
+	ante "github.com/cosmos/cosmos-sdk/x/auth/ante"
 	"github.com/McDaan/testchain/x/erc20"
 	erc20client "github.com/McDaan/testchain/x/erc20/client"
 	erc20keeper "github.com/McDaan/testchain/x/erc20/keeper"
