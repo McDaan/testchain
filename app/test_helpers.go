a0344225582516e9630bb5714518ba2085dc8d8f
Mandragora <dan.investor.1996@gmail.com>
2023-02-13 20:32:42 +0100

    Update test_helpers.go

--- app/test_helpers.go
+++ app/test_helpers.go
@@ -97,9 +97,9 @@ func Setup(
 }
 
 // SetupTestingApp initializes the IBC-go testing application
-func SetupTestingApp() (ibctesting.TestingApp, map[string]json.RawMessage, opts ...wasm.Option) {
+func SetupTestingApp() (ibctesting.TestingApp, map[string]json.RawMessage, op ...wasm.Option) {
 	db := dbm.NewMemDB()
 	cfg := encoding.MakeConfig(ModuleBasics)
-	app := NewTestChain(log.NewNopLogger(), db, nil, true, map[int64]bool{}, DefaultNodeHome, 5, cfg, wasm.EnableAllProposals, simapp.EmptyAppOptions{}, opts)
+	app := NewTestChain(log.NewNopLogger(), db, nil, true, map[int64]bool{}, DefaultNodeHome, 5, cfg, wasm.EnableAllProposals, simapp.EmptyAppOptions{}, op)
 	return app, NewDefaultGenesisState()
 }
