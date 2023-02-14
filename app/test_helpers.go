ab3834bd9cf5fb6b15d271c462c757aa0dea62ad
Mandragora <dan.investor.1996@gmail.com>
2023-02-14 08:31:21 +0100

    Update test_helpers.go

--- app/test_helpers.go
+++ app/test_helpers.go
@@ -6,7 +6,7 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	ibctesting "github.com/cosmos/ibc-go/v3/testing"
-	"github.com/cosmos/ibc-go/v3/testing/simapp"
+	//"github.com/cosmos/ibc-go/v3/testing/simapp"
 	simappparams "github.com/cosmos/cosmos-sdk/simapp/params"
 	abci "github.com/tendermint/tendermint/abci/types"
 	"github.com/tendermint/tendermint/libs/log"
@@ -20,7 +20,7 @@ import (
 
 	"github.com/McDaan/testchain/cmd/config"
 	"github.com/CosmWasm/wasmd/x/wasm"
-	//"github.com/CosmWasm/wasmd/app/params"
+	"github.com/CosmWasm/wasmd/app/params"
 	//"github.com/cosmos/cosmos-sdk/simapp"
 )
 
