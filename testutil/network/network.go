106dd0e68c5382d0ace4e5b576c18cf8d7e6459b
Mandragora <dan.investor.1996@gmail.com>
2023-02-14 10:08:56 +0100

    Update network.go

--- testutil/network/network.go
+++ testutil/network/network.go
@@ -55,7 +55,7 @@ import (
 	evmtypes "github.com/evmos/ethermint/x/evm/types"
 	
 	//wasmappparams "github.com/CosmWasm/wasmd/app/params"
-	//wasmapp "github.com/CosmWasm/wasmd/app"
+	/wasmapp "github.com/CosmWasm/wasmd/app"
 	//"github.com/CosmWasm/wasmd/x/wasm"
 )
 
@@ -129,7 +129,7 @@ func DefaultConfig() Config {
 }
 
 // NewAppConstructor returns a new Evmos AppConstructor
-func NewAppConstructor(encodingCfg params.EncodingConfig) AppConstructor {
+func NewAppConstructor(encodingCfg wasmapp.EncodingConfig) AppConstructor {
 	return func(val Validator) servertypes.Application {
 		return app.NewTestChain(
 			val.Ctx.Logger, dbm.NewMemDB(), nil, true, make(map[int64]bool), val.Ctx.Config.RootDir, 0,
