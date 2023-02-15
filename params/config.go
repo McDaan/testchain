454de3ceeb252e26d60570560b9dd7d77b9df3a6
Mandragora <dan.investor.1996@gmail.com>
2023-02-15 18:22:12 +0100

    Update config.go

--- params/config.go
+++ params/config.go
@@ -22,7 +22,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/types/module"
 	"github.com/cosmos/cosmos-sdk/x/auth/tx"
 
-	enccodec "github.com/evmos/ethermint/encoding/codec"
+	enccodec "github.com/McDaan/testing/encoding/codec"
 )
 
 // MakeConfig creates an EncodingConfig for testing
